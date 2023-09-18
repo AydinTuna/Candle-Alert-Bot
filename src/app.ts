14493bebe3e89c4e312418ceb894b5aab2884f9f
benbaho <aydinbahadirtuna@gmail.com>
2023-09-18 16:27:20 +0300

    .

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const commandList: { [key: string]: string } = {
     help: "help"
 }
 
-cron.schedule('55 2, 6, 10, 14, 18, 22 * * *', () => {
+cron.schedule('*/2 * * * *', () => {
 
     for (const userId in h4AlertUsers) {
         bot.telegram.sendMessage(Number(userId), "🚀 Check the stock market, H4 candle has closed! 📈");
@@ -22,8 +22,6 @@ cron.schedule('55 2, 6, 10, 14, 18, 22 * * *', () => {
 });
 
 bot.command(commandList.h4Alert, (ctx: Context) => {
-    console.log("hi");
-
     const userId = ctx.message?.from?.id;
     if (userId) {
         h4AlertUsers[userId] = true;
